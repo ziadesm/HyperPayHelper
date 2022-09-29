4fe837a90231c33ca6cceae613e42dcb4e9f493d
Ziad <zi.eus@hotmail.com>
2022-09-29 16:25:35 +0200

    update and finish gradle version

--- HyperPay/src/main/java/com/app/hyperpay/helper_model/PaymentResponse.kt
+++ HyperPay/src/main/java/com/app/hyperpay/helper_model/PaymentResponse.kt
@@ -1,6 +1,4 @@
 package com.app.hyperpay.helper_model
-import com.oppwa.mobile.connect.exception.PaymentError
-import com.oppwa.mobile.connect.provider.Transaction
 
 sealed class PaymentResponse {
     data class PaymentSuccess(val transaction: PaymentSuccessModel?, val resourcePath: String?): PaymentResponse()
