c92403aaa106849159c13450b9af788169b0add6
Ziad <zi.eus@hotmail.com>
2022-10-13 13:31:53 +0200

    update

--- app/src/main/java/com/app/hyperpayandroiddemo/MainActivity.kt
+++ app/src/main/java/com/app/hyperpayandroiddemo/MainActivity.kt
@@ -6,7 +6,7 @@ import com.app.hyperpay.builder.OnPaymentResponseCallback
 import com.app.hyperpay.helper_model.PaymentResponse
 import com.app.hyperpay.temp.PaymentHelper
 
-class MainActivity : AppCompatActivity(), OnPaymentResponseCallback {/*OnPaymentResponseCallback*/
+class MainActivity : AppCompatActivity(), OnPaymentResponseCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,24 +15,16 @@ class MainActivity : AppCompatActivity(), OnPaymentResponseCallback {/*OnPayment
             .paymentType(hashSetOf("VISA"))
             .checkoutId("80FB22874855575586E1BD0CC2489E1E.uat01-vm-tx01")
             .shopperResultUrl("com.app.hyperpayandroiddemo")
+            .testMode(false)
             .build()
+
+        PaymentHelper.Builder(this, this)
+
     }
 
+
     override fun onPaymentResponseCallback(paymentResponse: PaymentResponse) {
-        when(paymentResponse) {
-            is PaymentResponse.PaymentSuccess -> {
-                Log.e("PaymentResult", "onActivityResult: SUCCESS >>>>> ${paymentResponse.resourcePath}")
-                Log.e("PaymentResult", "onActivityResult: SUCCESS >>>>> ${paymentResponse.transaction?.transaction_type}")
-            }
-            is PaymentResponse.PaymentCanceled -> {
-                Log.e("PaymentResult", "onActivityResult: CANCELED >>>>> CANCEL")
-            }
-            is PaymentResponse.PaymentFailed -> {
-                Log.e("PaymentResult", "onActivityResult: ERROR >>>>> ${paymentResponse.paymentError?.error_code}")
-                Log.e("PaymentResult", "onActivityResult: ERROR >>>>> ${paymentResponse.paymentError?.error_info}")
-                Log.e("PaymentResult", "onActivityResult: ERROR >>>>> ${paymentResponse.paymentError?.error_message}")
-            }
-        }
+
     }
 
 }
