c972ef1f7bb0c6bf7ac244301ad55fee6fcdfef7
Ziad <zi.eus@hotmail.com>
2022-10-16 12:57:25 +0200

    update gradle

---
+++ acceptsdk-release/build/generated/source/buildConfig/release/com/app/hyperpay/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.app.hyperpay;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "com.app.hyperpay";
+  public static final String BUILD_TYPE = "release";
+}
